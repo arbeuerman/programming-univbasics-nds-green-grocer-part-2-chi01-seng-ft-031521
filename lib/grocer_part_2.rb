ef311cc9cb5ee397d1637fa7ba95125dc5ae6fc0
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-13 18:24:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -49,21 +49,21 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   consolidated_cart = consolidate_cart(cart)
-  binding.pry 
+  #binding.pry 
   consolidated_cart_w_coupons = apply_coupons(consolidated_cart, coupons)
-  binding.pry 
+  #binding.pry 
   updated_prices_cart = apply_clearance(consolidated_cart_w_coupons)
-  binding.pry 
+  #binding.pry 
   total_cost = 0
   updated_prices_cart.each do |item_with_updated_price|
     cost_of_item = item_with_updated_price[:count] * item_with_updated_price[:price]
     total_cost += cost_of_item  
   end 
-  binding.pry 
+  #binding.pry 
   if total_cost > 100
     total_cost *= 0.90
   end   
-  binding.pry 
+  #binding.pry 
   total_cost
 end
 
