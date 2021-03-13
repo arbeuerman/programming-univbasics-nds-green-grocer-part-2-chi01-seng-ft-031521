f7b7f04939b4e9c34ff879060a5ea0538879e5a3
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-13 18:25:28 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -2,7 +2,6 @@ require_relative './part_1_solution.rb'
 require 'pry'
 
 def apply_coupons(consolidated_cart, coupons)
-  #binding.pry 
   consolidated_cart.each do |consolidated_item|
     item_with_coupon = find_item_by_name_in_collection(consolidated_item[:item], coupons)
     if item_with_coupon != nil
@@ -12,7 +11,6 @@ def apply_coupons(consolidated_cart, coupons)
       item_coupon_applied[:item] = item_with_coupon[:item] + " W/COUPON"
       #update count of original item and remove item if count is 0
       #do nothing if there are fewer items than coupon is applicable for
-      #binding.pry
       if consolidated_item[:count] >= item_with_coupon[:num]
         consolidated_item[:count] -= item_with_coupon[:num]
         #update price per item from coupon 
@@ -21,11 +19,9 @@ def apply_coupons(consolidated_cart, coupons)
         item_coupon_applied[:clearance] = consolidated_item[:clearance] 
         item_coupon_applied[:count] = item_with_coupon[:num]
         consolidated_cart << item_coupon_applied
-        #binding.pry
       end
     end 
   end 
-#binding.pry
 consolidated_cart
 end
 
@@ -39,47 +35,16 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
   consolidated_cart = consolidate_cart(cart)
-  #binding.pry 
   consolidated_cart_w_coupons = apply_coupons(consolidated_cart, coupons)
-  #binding.pry 
   updated_prices_cart = apply_clearance(consolidated_cart_w_coupons)
-  #binding.pry 
   total_cost = 0
   updated_prices_cart.each do |item_with_updated_price|
     cost_of_item = item_with_updated_price[:count] * item_with_updated_price[:price]
     total_cost += cost_of_item  
   end 
-  #binding.pry 
   if total_cost > 100
     total_cost *= 0.90
   end   
-  #binding.pry 
   total_cost
 end
-
-cart = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 1},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 1},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 1},
-  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
-]
-
-coupons = [
-      {:item => "AVOCADO", :num => 2, :cost => 5.00},
-      {:item => "BEER", :num => 2, :cost => 20.00},
-      {:item => "CHEESE", :num => 3, :cost => 15.00}
-    ]
-
-#puts apply_coupons(consolidated_cart, coupons)
-#puts apply_clearance(consolidated_cart)
-puts checkout(cart, coupons)
